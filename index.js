6800f6385411aee9cdb84633883648ac80d932e5
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-13 16:54:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments)
 {
   var pairedskills = [];
-  for (counter = 0; counter < musicians.length(); counter++)
+  for (let counter = 0; counter < musicians.length(); counter++)
   {
     skill = `${musicians[counter]} plays ${instruments[counter]}`;
     empty[counter] = skill;
